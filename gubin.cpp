67fb497d5f59474de613537027222bc0d2b8f11a
Leonid Gubin <gubin_la@mail.ru>
2026-09-24 21:45:33 +0700

    feat: перенесены функции варианта 72, добавлены пункты меню 1-3

---
+++ gubin.cpp
@@ -0,0 +1,16 @@
+#include "gubin.h"
+
+double daysToHours(double d)
+{
+	return d * 24;
+}
+
+double hoursToMinutes(double h)
+{
+	return h * 60;
+}
+
+double minutesToDays(double d)
+{
+	return d / 1440;
+}
---
+++ gubin.h
@@ -0,0 +1,9 @@
+#ifndef GUBIN_H
+#define GUBIN_H
+
+// Функции варианта 72 (Губин Л. А.)
+double daysToHours(double d);
+double hoursToMinutes(double h);
+double minutesToDays(double d);
+
+#endif // GUBIN_H
